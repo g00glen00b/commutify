ac3155643df89e7a7f66bc3b805f62d5ccf2d973
g00glen00b <me@g00glen00b.be>
2016-02-20 10:19:04 +0100

    Project setup

---
+++ src/main/java/be/g00glen00b/commutify/CommutifyWebappApplication.java
@@ -0,0 +1,12 @@
+package be.g00glen00b.commutify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommutifyWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommutifyWebappApplication.class, args);
+	}
+}
---
+++ src/main/java/be/g00glen00b/commutify/be/g00glen00b/commutify/entity/Profile.java
@@ -0,0 +1,4 @@
+package be.g00glen00b.commutify.be.g00glen00b.commutify.entity;
+
+public class Profile {
+}
